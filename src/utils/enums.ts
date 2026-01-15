f379e70d5e6116361f4019f8a066e6f4a6c3fb28
anmol420 <anmolanand2k15@gmail.com>
2026-01-15 22:29:25 +0530

    feat: added enums

---
+++ src/utils/enums.ts
@@ -0,0 +1,11 @@
+export enum UserRole {
+  CUSTOMER = "customer",
+  VENDOR = "vendor",
+  ADMIN = "admin",
+}
+
+export enum UserStatus {
+  ACTIVE = "active",
+  SUSPENDED = "suspended",
+  DELETED = "deleted",
+}
